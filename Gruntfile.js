8bb0ce0f8c65905b446745bfbcec8cfb058c3537
Ruben de Laat <ruben@logic-labs.nl>
2016-01-06 13:03:16 +0100

    test

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        concat: {
+            js: {
+                files: {
+                    "js/bimserverapi.js": ["js/*.js"]
+                },
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    "js/bimserverapi.min.js": ["bimserverapi.js"]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-css");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+
+    grunt.registerTask("default", ["concat", "uglify"]);
+};
