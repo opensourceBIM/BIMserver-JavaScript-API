23a974b1d4888e3ea56c3be40ca7b022113dbd95
Ruben de Laat <ruben@logic-labs.nl>
2016-01-07 12:07:39 +0100

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,9 +15,29 @@ module.exports = function(grunt) {
                     "js/bimserverapi.min.js": ["js/bimserverapi.js"]
                 }
             }
-        }
+        },
+        github_releaser: {
+            options: {
+              repository: 'opensourceBIM/BIMserver-JavaScript-API',
+              auth: {
+                user: '$USERNAME$',
+                password: '$PASSWORD$'
+              },
+              release: {
+                tag_name: 'version1',
+                name: 'jelly-bean',
+                body: 'Description of the release',
+                draft: false, 
+                prerelease: true
+              }
+            },
+            files: {
+              'dest': ['release.zip'],
+            },
+          }
     });
 
+    grunt.loadNpmTasks("grunt-github-releaser");
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-css");
