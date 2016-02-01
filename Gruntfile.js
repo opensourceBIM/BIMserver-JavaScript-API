952c4ccc7332ff900fda78f2db53d00f81043ace
Ruben de Laat <ruben@logic-labs.nl>
2016-02-01 14:29:05 +0100

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,5 +54,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-zip");
 
-	grunt.registerTask("default", [ "concat", "uglify", "zip", "github-release" ]);
+	grunt.registerTask("default", [ "concat", "uglify"]);
 };
