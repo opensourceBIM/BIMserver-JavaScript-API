24b88b67890afc28dc567c04ddf5760bbb177a27
Ruben de Laat <ruben@logic-labs.nl>
2016-04-06 12:21:49 +0200

    removed github releaser

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,29 +27,9 @@ module.exports = function(grunt) {
 		},
 		zip: {
 			"output/bimserverapi-source-%VERSION%.zip": ["js/*.js"]
-		},
-		"github-release" : {
-			options : {
-				repository : "opensourceBIM/BIMserver-JavaScript-API",
-				auth : {
-					user : "%USERNAME%",
-					password : "%PASSWORD%"
-				},
-				release : {
-					tag_name : "%VERSION%",
-					name : "BIMServer JavaScript API %VERSION%",
-					body : "Testing...",
-					draft : false,
-					prerelease : true
-				}
-			},
-			files : {
-				src : [ "output/bimserverapi-%VERSION%.min.js", "output/bimserverapi-%VERSION%.js", "output/bimserverapi-source-%VERSION%.zip" ]
-			}
 		}
 	});
 
-	grunt.loadNpmTasks("grunt-github-releaser");
 	grunt.loadNpmTasks("grunt-contrib-concat");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-zip");
