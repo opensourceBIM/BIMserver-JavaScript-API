3d22e2b8f4b27ce3c86ca4b2536894a4a1886a2f
Ruben de Laat <Ruben de Laat@134.221.248.203>
2018-01-12 12:46:15 +0100

    Removed js/

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 		concat : {
 			js : {
 				files : {
-					"output/bimserverapi-%VERSION%.js" : [ "js/bimserverapipromise.js", "js/bimserverapiwebsocket.js", "js/geometry.js", "js/ifc4.js", "js/ifc2x3tc1.js", "js/model.js", "js/translations_en.js", "js/bimserverclient.js" ]
+					"output/bimserverapi-%VERSION%.js" : [ "bimserverapipromise.js", "bimserverapiwebsocket.js", "geometry.js", "ifc4.js", "ifc2x3tc1.js", "model.js", "translations_en.js", "bimserverclient.js" ]
 				},
 			}
 		},
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
 			}
 		},
 		zip: {
-			"output/bimserverapi-source-%VERSION%.zip": ["js/*.js"]
+			"output/bimserverapi-source-%VERSION%.zip": ["bimserverapipromise.js", "bimserverapiwebsocket.js", "geometry.js", "ifc4.js", "ifc2x3tc1.js", "model.js", "translations_en.js", "bimserverclient.js"]
 		}
 	});
 
