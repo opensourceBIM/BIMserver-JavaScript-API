71cedbaab7620f22f761c3b42e0d37fbf62b0749
Ruben de Laat <ruben@logic-labs.nl>
2020-01-30 16:32:47 +0100

    Newly generated json schema files

--- geometry.js
+++ geometry.js
@@ -1,81 +1,352 @@
 export const geometry = {
-	"classes": {
-		"Vector3f": {
-			"domain": "bimserver",
-			"superclasses": [],
-			"fields": {
-				"x": {
-					"type": "float",
-					"reference": false,
-					"many": false
-				},
-				"y": {
-					"type": "float",
-					"reference": false,
-					"many": false
-				},
-				"z": {
-					"type": "float",
-					"reference": false,
-					"many": false
-				},
-			}
-		},
-		"GeometryData": {
-			"domain": "bimserver",
-			"superclasses": [],
-			"fields": {}
-		},
-		"GeometryInfo": {
-			"domain": "bimserver",
-			"superclasses": [],
-			"fields": {
-				"minBounds": {
-					"type": "Vector3f",
-					"reference": true,
-					"many": false
-				},
-				"maxBounds": {
-					"type": "Vector3f",
-					"reference": true,
-					"many": false
-				},
-				"startVertex": {
-					"type": "int",
-					"reference": false,
-					"many": false
-				},
-				"startIndex": {
-					"type": "int",
-					"reference": false,
-					"many": false
-				},
-				"primitiveCount": {
-					"type": "int",
-					"reference": false,
-					"many": false
-				},
-				"data": {
-					"type": "GeometryData",
-					"reference": true,
-					"many": false
-				},
-				"transformation": {
-					"type": "float",
-					"reference": false,
-					"many": true
-				},
-				"area": {
-					"type": "float",
-					"reference": false,
-					"many": false
-				},
-				"volume": {
-					"type": "float",
-					"reference": false,
-					"many": false
-				}
-			}
-		}
-	}
-};
+		  "classes" : {
+			    "GeometryInfo" : {
+			      "domain" : "geometry",
+			      "superclasses" : [ ],
+			      "fields" : {
+			        "bounds" : {
+			          "type" : "Bounds",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "boundsUntransformed" : {
+			          "type" : "Bounds",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "startVertex" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "startIndex" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "primitiveCount" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "transformation" : {
+			          "type" : "bytearray",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "data" : {
+			          "type" : "GeometryData",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "area" : {
+			          "type" : "double",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "volume" : {
+			          "type" : "double",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "hasTransparency" : {
+			          "type" : "boolean",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "ifcProductOid" : {
+			          "type" : "long",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "ifcProductUuid" : {
+			          "type" : "bytearray",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "ifcProductRid" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "density" : {
+			          "type" : "EFloat",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "boundsMm" : {
+			          "type" : "Bounds",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "boundsUntransformedMm" : {
+			          "type" : "Bounds",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "additionalData" : {
+			          "type" : "string",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "nrColors" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "nrVertices" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "ifcProductPid" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        }
+			      }
+			    },
+			    "Vector3f" : {
+			      "domain" : "geometry",
+			      "superclasses" : [ ],
+			      "fields" : {
+			        "x" : {
+			          "type" : "double",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "y" : {
+			          "type" : "double",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "z" : {
+			          "type" : "double",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        }
+			      }
+			    },
+			    "Bounds" : {
+			      "domain" : "geometry",
+			      "superclasses" : [ ],
+			      "fields" : {
+			        "min" : {
+			          "type" : "Vector3f",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "max" : {
+			          "type" : "Vector3f",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        }
+			      }
+			    },
+			    "Buffer" : {
+			      "domain" : "geometry",
+			      "superclasses" : [ ],
+			      "fields" : {
+			        "data" : {
+			          "type" : "bytearray",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        }
+			      }
+			    },
+			    "GeometryData" : {
+			      "domain" : "geometry",
+			      "superclasses" : [ ],
+			      "fields" : {
+			        "nrIndices" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "nrVertices" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "nrNormals" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "nrColors" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "indices" : {
+			          "type" : "Buffer",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "vertices" : {
+			          "type" : "Buffer",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "verticesQuantized" : {
+			          "type" : "Buffer",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "normals" : {
+			          "type" : "Buffer",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "normalsQuantized" : {
+			          "type" : "Buffer",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "colorsQuantized" : {
+			          "type" : "Buffer",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "color" : {
+			          "type" : "Vector4f",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "hasTransparency" : {
+			          "type" : "boolean",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "reused" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "type" : {
+			          "type" : "short",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "mostUsedColor" : {
+			          "type" : "Vector4f",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "boundsMm" : {
+			          "type" : "Bounds",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "saveableTriangles" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "colorPack" : {
+			          "type" : "ColorPack",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "lineIndices" : {
+			          "type" : "Buffer",
+			          "reference" : true,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "nrLineIndices" : {
+			          "type" : "int",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        }
+			      }
+			    },
+			    "Vector4f" : {
+			      "domain" : "geometry",
+			      "superclasses" : [ ],
+			      "fields" : {
+			        "x" : {
+			          "type" : "EFloat",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "y" : {
+			          "type" : "EFloat",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "z" : {
+			          "type" : "EFloat",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        },
+			        "w" : {
+			          "type" : "EFloat",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        }
+			      }
+			    },
+			    "ColorPack" : {
+			      "domain" : "geometry",
+			      "superclasses" : [ ],
+			      "fields" : {
+			        "data" : {
+			          "type" : "bytearray",
+			          "reference" : false,
+			          "many" : false,
+			          "inverse" : false
+			        }
+			      }
+			    }
+			  }
+			};
